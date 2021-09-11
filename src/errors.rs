8da300c12caab87866b1b86158916b6d7578b3ea
alberto tirla <albertotirla@gmail.com>
2021-09-12 01:29:10 +0300

    fix error due to not committing properly

--- src/errors.rs
+++ src/errors.rs
@@ -1,19 +1,19 @@
 
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, thiserror::Error)]
-    #[non_exhaustive]
-    pub enum Error {
-        #[error("unable to initialise speech dispatcher")]
-        InitError,
-        #[error("error synthesizing text")]
-        SpeechSynthError,
-        #[error("unable to stop currently spoken message. Perhaps there's no message being spoken?")]
-        StopSpeechError,
-        #[error("unable to cancel speech")]
-        SpeechCancelationError,
-        #[error("error pausing or resuming speech")]
-        TTSPauseResumeError,
-        #[error("problems setting a speech param")]
-        SynthParamError,
-        #[error("a weird unknown error")]
-        Unknown,
-    }
+#[derive(Debug, Clone, Copy, PartialEq, Eq, thiserror::Error)]
+#[non_exhaustive]
+pub enum Error {
+    #[error("unable to initialise speech dispatcher")]
+    InitError,
+    #[error("error synthesizing text")]
+    SpeechSynthError,
+    #[error("unable to stop currently spoken message. Perhaps there's no message being spoken?")]
+    StopSpeechError,
+    #[error("unable to cancel speech")]
+    SpeechCancelationError,
+    #[error("error pausing or resuming speech")]
+    TTSPauseResumeError,
+    #[error("problems setting a speech param")]
+    SynthParamError,
+    #[error("a weird unknown error")]
+    Unknown,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 mod tts;
 pub use tts::Speaker;
-mod  errors;
+mod errors;
 pub use errors::Error;
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]    
-    fn succesfull_init(){
-        let res=Speaker::init("tester");
-        assert_eq!(res, Ok);
+    #[test]
+    fn succesfull_init() {
+        let res = Speaker::init("tester");
+        assert_eq!(res.is_err(), false)
     }
     #[test]
-    #[should_panic(expected="using this initialiser with a slice of characters with null bytes in it should fail")]
-    fn this_test_should_fail(){
-        let speech=Speaker::init("tes\0ter");
+    #[should_panic(expected = "should not contain null bytes")]
+    fn this_test_should_fail() {
+        let _speech = Speaker::init("tes\0ter");
     }
 }
