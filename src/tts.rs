865965f64e88eec9fb03356b57284580031b300a
alberto tirla <albertotirla@gmail.com>
2021-09-11 19:59:03 +0300

    add tests and formatting fixes

--- src/tts.rs
+++ src/tts.rs
@@ -1,9 +1,10 @@
-use std::{ffi::CString, ptr};
+use crate::Error;
 use speech_dispatcher_sys as spd;
-use crate::errors::error;
+use std::{ffi::CString, ptr};
+#[derive(Debug, PartialEq)]
 pub struct Speaker {
     con: *mut spd::SPDConnection,
-    }
+}
 impl Speaker {
     pub fn init(app_name: &str) -> Result<Speaker, Error> {
         let name = CString::new(app_name).expect("name should not contain null bytes");
@@ -15,17 +16,17 @@ impl Speaker {
                 spd::SPDConnectionMode::SPD_MODE_SINGLE,
             )
         };
-        
-        if con.is_null(){
+
+        if con.is_null() {
             return Err(Error::InitError);
         }
         let speaker = Speaker { con };
-        Ok(speaker)    
+        Ok(speaker)
     }
 }
 impl Drop for Speaker {
-    fn drop(&mut self){
-        unsafe{
+    fn drop(&mut self) {
+        unsafe {
             spd::spd_close(self.con);
         }
     }
