a11cea9b16eb4f042eb3b1bc0ee306555888c22e
alberto tirla <albertotirla@gmail.com>
2021-09-19 20:59:30 +0300

    add rate, volume and pitch controlling functions to Speaker

--- src/tts.rs
+++ src/tts.rs
@@ -6,6 +6,17 @@ use std::{
     ptr::{self, NonNull},
 };
 
+//macros for defining functions in shorter code and with less repetition
+macro_rules! spd_return_err_if_fail {
+    ($res: ident, $err: ident) => {
+        if $res == -1 {
+            Err(Error::$err)
+        } else {
+            Ok(())
+        }
+    };
+}
+
 //enums for tts and speech dispatcher specific things
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 #[repr(u32)]
@@ -21,16 +32,6 @@ pub enum Priority {
     Notification = spd::SPDPriority::SPD_NOTIFICATION,
     Progress = spd::SPDPriority::SPD_PROGRESS,
     Text = spd::SPDPriority::SPD_TEXT,
-}
-
-macro_rules! spd_return_err_if_fail {
-    ($res: ident, $err: ident) => {
-        if $res == -1 {
-            Err(Error::$err)
-        } else {
-            Ok(())
-        }
-    };
 }
 
 #[derive(Debug, PartialEq)]
@@ -64,7 +65,6 @@ impl Speaker {
     pub fn speak_char(&self, priority: Priority, c: char) -> Result<(), Error> {
         // I'd love to ues spd_wchar to make this easier, but it seems that it doesn't work, at
         // least not with espeak-ng. (mcb2003 <mikey@blindcomputing.org>)
-
         // This is 4-bytes for the char plus a null terminator
         let mut buf = [0u8; 5];
         // Encode the character into `buf` as UTF8.
@@ -134,12 +134,44 @@ impl Speaker {
             .ok_or(Error::ListSynthesisVoicesError)?;
         Ok(iter::SynthesisVoiceIter::new(res))
     }
+        pub fn rate(&self) -> i32 {
+        let res = unsafe { spd::spd_get_voice_rate(self.con.as_ptr()) };
+        res
+    }
+
+    pub fn set_rate(&mut self, value: i32) -> Result<(), Error> {
+        let res = unsafe { spd::spd_set_voice_rate(self.con.as_mut(), value) };
+        spd_return_err_if_fail!(res, SynthParamError)
+    }
+    pub fn volume(&self) -> i32 {
+        let res = unsafe { spd::spd_get_volume(self.con.as_ptr()) };
+        res
+    }
+
+    pub fn set_volume(&mut self, value: i32) -> Result<(), Error> {
+        let res = unsafe { spd::spd_set_volume(self.con.as_mut(), value) };
+        spd_return_err_if_fail!(res, SynthParamError)
+    }
+    pub fn pitch(&self) -> i32 {
+        let res = unsafe { spd::spd_get_voice_pitch(self.con.as_ptr()) };
+        res
+    }
+
+    pub fn set_pitch(&mut self, value: i32) -> Result<(), Error> {
+        let res = unsafe { spd::spd_set_voice_pitch(self.con.as_mut(), value) };
+        spd_return_err_if_fail!(res, SynthParamError)
+    }
+    
+    pub fn set_pitch_range(&mut self, value: i32) -> Result<(), Error> {
+        let res = unsafe { spd::spd_set_voice_pitch_range(self.con.as_mut(), value) };
+        spd_return_err_if_fail!(res, SynthParamError)
+    }
 }
 
 impl Drop for Speaker {
     fn drop(&mut self) {
         unsafe {
-            spd::spd_close(self.con.as_ptr());
+            spd::spd_close(self.con.as_mut());
         }
     }
 }
