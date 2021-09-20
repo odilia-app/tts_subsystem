fb338e2f32d90c9c863dad2e2aa9a7166dbd1c01
mcb2003 <mikeybuchan@hotmail.co.uk>
2021-09-20 19:27:28 +0100

    Add SynthesisVoiceIter.

--- src/iter.rs
+++ src/iter.rs
@@ -1,5 +1,9 @@
 use std::{borrow::Cow, ffi::CStr, marker::PhantomData, os::raw::c_char, ptr::NonNull};
 
+use speech_dispatcher_sys as spd;
+
+use crate::Voice;
+
 pub struct OutputModuleIter<'a> {
     current: NonNull<*mut c_char>,
     _marker: PhantomData<&'a crate::Speaker>,
@@ -30,6 +34,37 @@ impl<'a> Iterator for OutputModuleIter<'a> {
                 cstr = CStr::from_ptr(current);
             }
             Some(cstr.to_string_lossy())
+        }
+    }
+}
+
+pub struct SynthesisVoiceIter<'a> {
+    current: NonNull<*mut spd::SPDVoice>,
+    _marker: PhantomData<&'a crate::Speaker>,
+}
+
+impl SynthesisVoiceIter<'_> {
+    pub(crate) fn new(start: NonNull<*mut spd::SPDVoice>) -> Self {
+        Self {
+            current: start,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'a> Iterator for SynthesisVoiceIter<'a> {
+    type Item = Voice<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = unsafe { *(self.current.as_ptr()) };
+        if current.is_null() {
+            None
+        } else {
+            unsafe {
+                // Safety: The `char **` will not be NULL, but the `char *` could be
+                self.current = NonNull::new_unchecked(self.current.as_ptr().offset(1));
+            }
+            Some(Voice::from(unsafe { *current }))
         }
     }
 }
